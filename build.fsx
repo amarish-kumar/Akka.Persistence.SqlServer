f0be479c456baa53d2a151f1743c28533bf5493a
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2016-12-17 18:33:17 -0500

    force

--- build.fsx
+++ build.fsx
@@ -191,7 +191,7 @@ Target "ActivateFinalTargets"  <| fun _ ->
 
 Target "DiagnoseDockerOnBuildAgent" <| fun _ ->
     let posh = PowerShell.Create() 
-                    .AddScript(@"Restart-Service docker -Confirm")
+                    .AddScript(@"Restart-Service docker -Force")
                     .AddScript(@"Invoke-WebRequest https://raw.githubusercontent.com/Microsoft/Virtualization-Documentation/master/windows-server-container-tools/Debug-ContainerHost/Debug-ContainerHost.ps1 -UseBasicParsing | Invoke-Expression | Export-CSV containerDebug.csv")
                     .AddScript(@"Get-EventLog -LogName Application -Source Docker -After (Get-Date).AddMinutes(-30)  | Sort-Object Time | Export-CSV last30minutes.csv")
                     .AddScript(@"cat last30minutes.csv")
