76f2b69bcfc32092fbc7f4f15fddf078d737c1eb
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2016-12-17 18:44:06 -0500

    try deleting any extraneous docker.pid

--- build.fsx
+++ build.fsx
@@ -190,14 +190,17 @@ Target "ActivateFinalTargets"  <| fun _ ->
     ActivateFinalTarget "TearDownDbContainer"
 
 Target "DiagnoseDockerOnBuildAgent" <| fun _ ->
-    let posh = PowerShell.Create() 
+    let posh = PowerShell.Create()             
+                    .AddScript(@"rm C:\ProgramData\docker.pid")
+                    .AddScript(@"Remove-ItemProperty -Path 'HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Virtualization\Containers' -Name VSmbDisableOplocks")
                     .AddScript(@"Restart-Service docker -Force")
-                    .AddScript(@"Invoke-WebRequest https://raw.githubusercontent.com/Microsoft/Virtualization-Documentation/master/windows-server-container-tools/Debug-ContainerHost/Debug-ContainerHost.ps1 -UseBasicParsing | Invoke-Expression | Export-CSV containerDebug.csv")
-                    .AddScript(@"cat containerDebug.csv")
-//                    .AddScript(@"Get-EventLog -LogName Application -Source Docker -After (Get-Date).AddMinutes(-30)  | Sort-Object Time | Export-CSV last30minutes.csv")
-//                    .AddScript(@"cat last30minutes.csv")
 
-//                    .AddScript(@"wget https://github.com/Microsoft/Virtualization-Documentation/raw/master/windows-server-container-tools/Debug-ContainerHost/Debug-ContainerHost.ps1 -UseBasicParsin | iex")
+//                    .AddScript(@"Invoke-WebRequest https://raw.githubusercontent.com/Microsoft/Virtualization-Documentation/master/windows-server-container-tools/Debug-ContainerHost/Debug-ContainerHost.ps1 -UseBasicParsing | Invoke-Expression | Export-CSV containerDebug.csv")
+//                    .AddScript(@"cat containerDebug.csv")
+
+                    .AddScript(@"Get-EventLog -LogName Application -Source Docker -After (Get-Date).AddMinutes(-30)  | Sort-Object Time | Export-CSV last30minutes.csv")
+                    .AddScript(@"cat last30minutes.csv")
+
     posh.Invoke() 
         |> Seq.iter (fun x -> logfn "%O" x)
     match posh.HadErrors with
