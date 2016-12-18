eb4dca0195fec3e3070b273857c22f4786997375
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2016-12-17 19:20:15 -0500

    run docker version

--- build.fsx
+++ build.fsx
@@ -194,10 +194,11 @@ Target "DiagnoseDockerOnBuildAgent" <| fun _ ->
 //                    .AddScript(@"rm C:\ProgramData\docker.pid")
 //                    .AddScript(@"Remove-ItemProperty -Path 'HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Virtualization\Containers' -Name VSmbDisableOplocks")
 //                    .AddScript(@"Restart-Service docker -Force")
-                    .AddScript(@"Invoke-WebRequest https://raw.githubusercontent.com/Microsoft/Virtualization-Documentation/master/windows-server-container-tools/Debug-ContainerHost/Debug-ContainerHost.ps1 -UseBasicParsing | Invoke-Expression")
+//                    .AddScript(@"Invoke-WebRequest https://raw.githubusercontent.com/Microsoft/Virtualization-Documentation/master/windows-server-container-tools/Debug-ContainerHost/Debug-ContainerHost.ps1 -UseBasicParsing | Invoke-Expression")
 //                    .AddScript(@"Start-Service docker -Confirm")                     
 //                    .AddScript(@"Get-EventLog -LogName Application -Source Docker -After (Get-Date).AddMinutes(-30)  | Sort-Object Time | Export-CSV last30minutes.csv")
 //                    .AddScript(@"cat last30minutes.csv")
+                .AddScript(@"docker version")
 
     posh.Invoke() 
         |> Seq.iter (fun x -> logfn "%O" x)
